b6aa9ac899a45ead176509584600ff082d670747
taeginote <ab315601@naver.com>
2022-12-31 22:37:27 +0900

    Add : level3 설게 파트 작성
     level3 설계 파트 작성 했습니다.

--- quizz/level3/level3.js
+++ quizz/level3/level3.js
@@ -6,3 +6,18 @@
 3. 인풋 바탕으로 단계별 한글로 함수 설계하기
 4. 단계별 구글링 키워드 생각하기
 */
+
+/*
+    설계
+    html tag 가져오는 단계
+    1. 재료명 input document로 가져오기
+    2. 용량 input document로 가져오기
+    3. 추가 버튼 document로 가져오기
+    4. table document로 가져오기
+
+    5. 재료명 input에 입력된 value 가져와야함
+    6. 용량 input에 입력된 value 가져와야함
+    7. 추가 button을 누르면 5번, 6번에서 가져온 value값을 table 태그에 맞게 넣는다.
+    8. table에서 관리에는 html tag button을 넣어야한다.
+
+*/
