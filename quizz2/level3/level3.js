00ee20b9b802e36968b4930fde408898b0e8f263
taeginote <ab315601@naver.com>
2023-01-01 16:16:15 +0900

    Feat : level3, level4 설계 추가

--- quizz2/level3/level3.js
+++ quizz2/level3/level3.js
@@ -8,16 +8,25 @@
 */
 
 /*
-    설계
+    * 요구사항 및 인풋* 
+    1. 재료명과 용량 text 입력
+    2. 추가 버튼 push
+    3. 입력한 재료명, 용량, 삭제버튼이 출력해야함
+    4. 삭제버튼을 누르면 추가된 list에서 삭제해야한다.
+
+    * 한글 설계 및 구글링 키워드*
     html tag 가져오는 단계
-    1. 재료명 input document로 가져오기
+    1. 재료명 input document로 가져오기 
+        (js input태그 값 가져오기)
     2. 용량 input document로 가져오기
+        (js input태그 값 가져오기)
     3. 추가 버튼 document로 가져오기
+        ()
     4. table document로 가져오기
+        ()
 
     5. 재료명 input에 입력된 value 가져와야함
     6. 용량 input에 입력된 value 가져와야함
     7. 추가 button을 누르면 5번, 6번에서 가져온 value값을 table 태그에 맞게 넣는다.
     8. table에서 관리에는 html tag button을 넣어야한다.
-
 */
