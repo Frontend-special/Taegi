06f96ba70849f12c2b05cc78075011bab3d33919
taeginote <ab315601@naver.com>
2023-01-06 17:11:18 +0900

    Add : 새로운 파일 추가 하였음

---
+++ taegi_answer/level3/level3.js
@@ -0,0 +1,56 @@
+/* 
+레시피추가하기
+*/
+
+/*
+  [구글링x 분석]
+  1. 새로운 new Map() 생성 const INGREDIENT_LIST
+*/
+
+/*
+  [구글링x 분석]
+  1. 파라미터 e가 tr로 추정
+  2. if로 tr에서 button이 아닌 다른걸 누르면 return
+  3. ingredient에 text를 가져온다?
+  4. umm...
+*/
+/*
+  [구글링 키워드]
+  1. js 해당 요소 클릭 비교
+  2. js html 태그 삭제
+  3. 
+*/
+
+/*
+  [구글링x 분석]
+  1. 파라미터 (e)는 form일 것이다.
+  2. ingredientValue, weightValue에 value값을 핳당한다.
+  3. 만약 객체에 재료명이 있으면 alert
+  4. 만약 재료명 무게 둘다 inputvalue가 없으면 alert
+  5. const tr 생성하고 tr만들어줌
+  6. 그 tr에 td 재료명, 용량, 삭제버튼 넣음
+  7. 그리고 set을 통해 데이터 저장
+  8. 54번째 줄부터는 이해 x
+*/
+/*
+  [구글링 키워드]
+  1. js로 객체 만들기
+  2. js로 객체 데이터 만들기
+  3. js로 html 추가
+  4. js로 .set(재료명, 용량)
+ */
+
+/*
+  1. $button 클릭 이벤트 추가
+  2. 만약  ul.children 즉 li가 한개 이상이면 ul을 리셋하고 시작함
+  3. INGREDIENT_LIST 내용을 foreach로 돌린다.
+  4. 그래서 INGREDIENT_LIST에 있는 데이터를 HTML에 li로 추가
+  5. li의 innerText에 재료, 무게 넣고
+  6. append를 통해 li tag? 추가? 하는거같습니다.
+*/
+/*
+  [구글링 키워드]
+  1. js html 태그 추가
+  2. js 객체 for문
+  6. js .create
+*/
